25bdd23df004c8c46b001ac6e24e939883e200ea
jordane-quincy <jordanequincy@gmail.com>
2016-11-08 10:42:09 +0100

    init exo2

---
+++ TD1_Exo2/src/com/github/jordane_quincy/Document.java
@@ -0,0 +1,24 @@
+package com.github.jordane_quincy;
+
+public class Document {
+
+	private int idUtilisateur;
+	private int dureeImpression;
+
+	public int getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public int getDureeImpression() {
+		return dureeImpression;
+	}
+
+	public void setDureeImpression(int dureeImpression) {
+		this.dureeImpression = dureeImpression;
+	}
+
+}
