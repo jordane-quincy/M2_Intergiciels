7da1133ec07c68440cac43b364f5562dd23678a3
jordane-quincy <jordanequincy@gmail.com>
2016-11-08 10:10:14 +0100

    ajout classe role

---
+++ TD1_Exo1/src/com/github/jordane_quincy/Role.java
@@ -0,0 +1,14 @@
+package com.github.jordane_quincy;
+
+public class Role {
+	String theRole;
+
+	public Role(String theRole) {
+		this.theRole = theRole;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [theRole=" + theRole + "]";
+	}
+}
