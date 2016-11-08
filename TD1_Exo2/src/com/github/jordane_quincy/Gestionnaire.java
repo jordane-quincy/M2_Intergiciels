e09931549b0fd33e639bc1da26c70dcc5e8c9074
jordane-quincy <jordanequincy@gmail.com>
2016-11-08 10:51:16 +0100

    fusion gestionnaire et imprimante

--- TD1_Exo2/src/com/github/jordane_quincy/Gestionnaire.java
+++ TD1_Exo2/src/com/github/jordane_quincy/Gestionnaire.java
@@ -1,5 +1,22 @@
 package com.github.jordane_quincy;
 
-public class Gestionnaire {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gestionnaire extends Thread {
+
+	private static final int TAILLE_FILE = 4;
+	private List<Document> fileAttente = new ArrayList<Document>(TAILLE_FILE);
+
+	@Override
+	public void run() {
+		System.out.println("Thread Gestionnaire");
+		while (true) {
+			try {
+				Thread.currentThread().sleep(100);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
 
 }
