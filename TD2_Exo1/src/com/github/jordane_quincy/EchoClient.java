27a3ae8972e4fbfe798a8984e23a1ee125749c37
jordane-quincy <jordanequincy@gmail.com>
2016-11-15 09:43:49 +0100

    it work

--- TD2_Exo1/src/com/github/jordane_quincy/EchoClient.java
+++ TD2_Exo1/src/com/github/jordane_quincy/EchoClient.java
@@ -1,35 +1,108 @@
 package com.github.jordane_quincy;
 
 import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.text.ParseException;
 
 public class EchoClient {
+
+	private final static String FILENAME = "produit.ser";
+
+	private static final String HOST = "localhost";
+	private static final int PORT = 8080;
+
 	public static void main(String[] args) {
 		Socket theSocket;
 		DataInputStream theInputStream;
 		DataInputStream userInput;
 		PrintStream theOutputStream;
 		String theLine;
+		// Produit produit = null;
+		//
+		// try {
+		// produit = readProduit();
+		// } catch (Exception e) {
+		// System.out.println("Création d'un nouveau produit");
+		// produit = new Produit("nom", 100);
+		//
+		// try {
+		// writeProduit(produit);
+		// } catch (Exception e1) {
+		// System.err.println("Exception a l'enregistrement :" +
+		// e1.getMessage());
+		// }
+		//
+		// }
+
+		Produit produit = new Produit("nom", 100);
+
+		System.out.println("Produit avant envoi : " + produit);
+
+		ObjectOutputStream oos;
+		ObjectInputStream ios;
 		try {
-			theSocket = new Socket(args[0], Integer.parseInt(args[1]));
-			theInputStream = new DataInputStream(theSocket.getInputStream());
-			theOutputStream = new PrintStream(theSocket.getOutputStream());
-			userInput = new DataInputStream(System.in);
-			while (true) {
-				theLine = userInput.readLine();
-				if (theLine.equals(".")) {
-					break;
+			theSocket = new Socket(HOST, PORT);
+			// theInputStream = new DataInputStream(theSocket.getInputStream());
+			// theOutputStream = new PrintStream(theSocket.getOutputStream());
+			// userInput = new DataInputStream(System.in);
+
+			oos = new ObjectOutputStream(theSocket.getOutputStream());
+			ios = new ObjectInputStream(theSocket.getInputStream());
+			// while (true) {
+			// theLine = userInput.readLine();
+			// if (theLine.equals(".")) {
+			// break;
+			// }
+			// theOutputStream.println(theLine);
+			// System.out.println(theInputStream.readLine());
+			// }
+
+			oos.writeObject(produit);
+
+			try {
+				Produit produitDuServeur = (Produit) ios.readObject();
+
+				if (produitDuServeur != null) {
+					System.out.println("produitDuServeur : " + produitDuServeur);
+				} else {
+					System.err.println("produitDuServeur est null :-/");
 				}
-				theOutputStream.println(theLine);
-				System.out.println(theInputStream.readLine());
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+
 		} catch (UnknownHostException e) {
 			System.err.println(e);
 		} catch (IOException e) {
 			System.err.println(e);
 		}
+	}
+
+	public static void writeProduit(Produit produit) throws FileNotFoundException, IOException, ParseException {
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(FILENAME));
+
+		objectOutputStream.writeObject(produit);
+
+		objectOutputStream.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Produit readProduit() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(FILENAME));
+
+		Produit produit = (Produit) objectInputStream.readObject();
+
+		objectInputStream.close();
+
+		return produit;
 	}
 }
--- TD2_Exo1/src/com/github/jordane_quincy/EchoServer.java
+++ TD2_Exo1/src/com/github/jordane_quincy/EchoServer.java
@@ -1,28 +1,60 @@
 package com.github.jordane_quincy;
 
-import java.io.DataInputStream;
-import java.io.PrintStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	void doService(Socket clientSocket) {
-		DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-		PrintStream out = new PrintStream(clientSocket.getOutputStream());
-		while (true) {
-			String theLine = in.readLine();
-			out.println(theLine);
+
+	private static final int PORT = 8080;
+
+	void doService(Socket clientSocket) throws IOException {
+		// DataInputStream in = new
+		// DataInputStream(clientSocket.getInputStream());
+		// PrintStream out = new PrintStream(clientSocket.getOutputStream());
+		// while (true) {
+		// String theLine = in.readLine();
+		// out.println(theLine);
+		// }
+
+		ObjectInputStream ios;
+		ObjectOutputStream oos;
+		try {
+			ios = new ObjectInputStream(clientSocket.getInputStream());
+			oos = new ObjectOutputStream(clientSocket.getOutputStream());
+
+			try {
+				Produit produitRecu = (Produit) ios.readObject();
+
+				if (produitRecu != null) {
+					System.out.println("produitRecu : " + produitRecu);
+
+					produitRecu.augmenter(15);
+
+					oos.writeObject(produitRecu);
+				} else {
+					System.err.println("produitRecu est null :-/");
+				}
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			System.err.println("outch : " + e);
 		}
+
 	}
 
 	public static void main(String[] args) {
 		EchoServer echoServ = new EchoServer();
 		ServerSocket listenSocket;
 		try {
-			listenSocket = new ServerSocket(Integer.parseInt(args[0])); // port
+			listenSocket = new ServerSocket(PORT);
 			while (true) {
 				Socket clientSocket = listenSocket.accept();
-				System.err.println("Connexion from:" + clientSocket.getInetAddress());
+				System.err.println("Connexion from : " + clientSocket.getInetAddress());
 				echoServ.doService(clientSocket);
 			}
 		} catch (Exception e) {
